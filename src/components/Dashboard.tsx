f431c09df295d24a4137fb738bcef276e3a73f2d
Maikol <michaelsonm99@gmail.com>
2025-11-13 10:58:22 +0300

    foi

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -239,7 +239,7 @@ function DashboardContent({ stats, lowStockProducts, recentMovements, onCardClic
       </div>
 
       {/* Stats Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-6">
         <div className="relative group">
           <div 
             onClick={() => onCardClick("products")}
