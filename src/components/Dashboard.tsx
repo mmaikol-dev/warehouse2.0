c6a6cac507a6361fe0531db8aad1842819d70bd4
Maikol <michaelsonm99@gmail.com>
2025-11-13 10:53:49 +0300

    fir

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -340,10 +340,7 @@ function DashboardContent({ stats, lowStockProducts, recentMovements, onCardClic
           </div>
           {/* Hover Popup */}
           <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-4 py-3 bg-gray-900 text-white text-sm rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none whitespace-nowrap z-10 shadow-lg">
-            <div className="font-semibold mb-1">Inventory Valuation</div>
-            <div>Estimated total value of current stock</div>
-            <div>Based on unit prices × quantities</div>
-            <div>Excludes reserved or damaged items</div>
+            
             <div className="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-t-4 border-transparent border-t-gray-900"></div>
           </div>
         </div>
